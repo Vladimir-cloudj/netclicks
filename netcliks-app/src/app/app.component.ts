a8c92f9e858087798047d4c6dc9671fa7dea6eee
Vladimir-cloudj <wizart88@gmail.com>
2025-08-12 15:02:33 +0400

    finish project

--- netcliks-app/src/app/app.component.ts
+++ netcliks-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Film } from './models/film.model';
+import { FILMS } from './constants/films.constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  public films: Film[] = FILMS;
 }
